3d55f3dba92d8e4213d36afddbc378844c8f3915
AceCape <birennahak99@gmail.com>
2024-04-19 20:04:35 +0530

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+export function addToCart(productId, productName) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+            productId: productId,
+            productName: productName,
+            quantity: 1
+        });
+    }
+}
