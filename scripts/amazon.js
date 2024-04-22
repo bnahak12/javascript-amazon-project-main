cee6203d8e6d8adede24471b3928eb7b622a89ef
AceCape <birennahak99@gmail.com>
2024-04-23 00:37:01 +0530

    Convert  products into classes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -13,14 +13,14 @@ products.forEach((product) => {
         <div class="product-name limit-text-to-2-lines">${product.name}</div>
 
         <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
+          <img class="product-rating-stars" src="${product.getStarsURL()}">
           <div class="product-rating-count link-primary">
             ${product.rating.count}
           </div>
         </div>
 
         <div class="product-price">
-          $${formatCurrency(product.priceCents)}
+          ${product.getPrice()}
         </div>
 
         <div class="product-quantity-container">
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from './checkOut/orderSummary.js';
 import { renderPaymentSummary } from './checkOut/paymentSummary.js';
 import { rendercheckOutHeader } from './checkOut/checkOutHeader.js';
-import '../data/cart-Oop.js';
+//import '../data/cart-Oop.js';
 
 rendercheckOutHeader();
 renderOrderSummary();
