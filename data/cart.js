fed96bec21a25f513542bf9abf4734e1055b57a2
AceCape <birennahak99@gmail.com>
2024-04-22 20:29:15 +0530

    TestCases Skipped

--- data/cart.js
+++ data/cart.js
@@ -1,20 +1,26 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-    cart = [
-        {
-            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-            productName: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-            quantity: 2,
-            deliveryOptionsId: '1'
-        },
-        {
-            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-            productName: 'Intermediate Size Basketball',
-            quantity: 1,
-            deliveryOptionsId: '3'
-        }
-    ]
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                productName: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+                quantity: 2,
+                deliveryOptionsId: '1'
+            },
+            {
+                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                productName: 'Intermediate Size Basketball',
+                quantity: 1,
+                deliveryOptionsId: '3'
+            }
+        ]
+    }
 }
 
 export function saveToStorage() {
