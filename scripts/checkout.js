b703baf9bc52c0be9e0260f61f53ab974ea807ee
AceCape <birennahak99@gmail.com>
2024-05-06 01:48:29 +0530

    Learnt Promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,9 +4,46 @@ import { rendercheckOutHeader } from './checkOut/checkOutHeader.js';
 //import '../data/cart-Oop.js';
 // import '../backend/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(() => {
+Promise.all([new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('resolve1');
+    });
+}), new Promise((resolve) => {
+    loadCart(() => {
+        resolve();
+    });
+})
+]).then((values) => {
+    console.log(values); // returns an array of resolved values from the mentioned promises
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+/* new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('resolve1');// return resolve1 and can be passed to next promise  
+    });
+}).then((value) => {
+    console.log(value);
+
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve('resolve2');
+        });
+    });
+}).then((resolve) => {
+    renderOrderSummary();
+    renderPaymentSummary();
+}); */
+
+/* loadProducts(() => {
+    loadCart(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
+}); */
+
 rendercheckOutHeader();
