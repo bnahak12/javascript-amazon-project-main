61a7c8c95358ca8466e1984dacccd720e3d189c2
AceCape <birennahak99@gmail.com>
2024-05-05 23:49:43 +0530

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderOrderSummary } from './checkOut/orderSummary.js';
 import { renderPaymentSummary } from './checkOut/paymentSummary.js';
 import { rendercheckOutHeader } from './checkOut/checkOutHeader.js';
 //import '../data/cart-Oop.js';
+import '../backend/backend-practice.js';
 
 rendercheckOutHeader();
 renderOrderSummary();
